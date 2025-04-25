5e03bc8c6c99758ef336a8484a3805d90466b4f5
Mr Nim <50592567+mrnim94@users.noreply.github.com>
2025-04-25 07:11:34 +0000

    fix route table

--- c3-04-route-table.tf
+++ c3-04-route-table.tf
@@ -9,7 +9,6 @@ resource "azurerm_route_table" "route_table" {
 
 resource "azurerm_route" "routes" {
   for_each               = var.routes
-  count                  = length(var.routes) > 0 ? 1 : 0
   name                   = each.key
   resource_group_name    = var.resource_group_name
   route_table_name       = azurerm_route_table.route_table.name
