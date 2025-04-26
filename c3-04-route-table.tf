bf6bb132ce3b3398f20315fd76b66d6bcb561cce
Mr Nim <50592567+mrnim94@users.noreply.github.com>
2025-04-26 07:09:28 +0000

    update route_table_name

--- c3-04-route-table.tf
+++ c3-04-route-table.tf
@@ -11,7 +11,7 @@ resource "azurerm_route" "routes" {
   for_each               = var.routes
   name                   = each.key
   resource_group_name    = var.resource_group_name
-  route_table_name       = azurerm_route_table.route_table.name
+  route_table_name       = azurerm_route_table.route_table[count.index].name
   address_prefix         = each.value.address_prefix
   next_hop_type          = each.value.next_hop_type
   next_hop_in_ip_address = each.value.next_hop_in_ip_address
